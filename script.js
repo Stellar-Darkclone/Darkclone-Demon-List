a085ad76c561f544fae61d8ea9f48a9d4048796e
Stellar-Darkclone <rynelechua@gmail.com>
2026-04-26 00:08:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -206,10 +206,9 @@ document.addEventListener("DOMContentLoaded", updateCounters);
   }
 })();
 
-// Mobile menu
+// Mobile Hamburger Menu
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-
 if (hamburger && navLinks) {
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
